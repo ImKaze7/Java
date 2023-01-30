58b1b4891dffe41b2a0e0eaff8273b1f5591b1a8
ImKaze7 <josefernandoramosalania3@gmail.com>
2023-01-30 11:54:14 -0500

    JavaVSC

---
+++ VSC_JAVA/Practica/src/App.java
@@ -0,0 +1,27 @@
+
+
+     public class Maein {
+    //@autor Kaze
+    public static void main(String[] args) {
+        String nombre;
+        int a, b, c;
+        nombre = "José Fernando";
+        int edad = 17;
+        a = 1;
+        b = 2;
+        c = 3;
+        System.out.println("nombre");
+        System.out.println("edad");
+        
+        edad=26;
+        System.out.println("edad");
+        System.out.println(a+b+c);
+        
+        var primernombre = "José";
+        var segundonombre = "Fernando";
+        var nombrecompleto = primernombre+segundonombre;
+        
+        System.out.println("Mis nombres: " + nombrecompleto);
+        }
+}
+
